5d744dbf5a1ec3b3961731d11a0b4010180bf771
ak-journey <mikhelson53@gmail.com>
2020-07-13 08:16:53 +0300

    First commit

---
+++ src/views/EventList.vue
@@ -0,0 +1,33 @@
+<template>
+  <div>
+    <h1>Event Listing</h1>
+    <EventCard v-for="event in events" :key="event.id" :event="event" />
+  </div>
+</template>
+
+<script>
+import EventCard from "../components/EventCard";
+import EventService from "../../src/services/EventService";
+
+export default {
+  components: {
+    EventCard
+  },
+  data() {
+    return {
+      events: []
+    };
+  },
+  created() {
+    EventService.getEvents()
+      .then(response => {
+        this.events = response.data;
+      })
+      .catch(error => {
+        console.log("There was an error:" + error.response);
+      });
+  }
+};
+</script>
+
+<style></style>
